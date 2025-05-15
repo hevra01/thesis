386a0d4fc28428e396b6ac5f3c0d111a35ef859c
Hevra Petekkaya <hpetekka@wks-21-0614>
2025-05-15 18:49:45 +0200

    added Variance preserving SDE

--- sde/sdes.py
+++ sde/sdes.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torch
 from abc import ABC, abstractmethod
+from utils import batch_linspace, copy_tensor_or_create
 
 class SDE(ABC, nn.Module):
     """
@@ -43,6 +44,160 @@ class SDE(ABC, nn.Module):
         """
 
 
-    def score(self, x, t):
-        """"
+    @torch.no_grad()
+    def _solve(
+        self,
+        x_start: torch.Tensor,
+        t_start: float = 1.0,
+        t_end: float = 1e-4,
+        steps: int = 1000,
+        stochastic: bool = True,
+        **score_kwargs,
+    ):
+        """Solve the SDE or ODE with an Euler(-Maruyama) solver.
+
+        Note that this can be used for either the forward or backward solve, depending on whether
+        t_start < t_end (forward) or t_start > t_end (reverse). Note that this method is not
+        appropriate for the forward SDE; the forward SDE should have an analytical solution.
+
+        TODO: Add predictor-corrector steps.
+
+        Args:
+            x_start (Tensor of shape (batch_size, ...)): The starting point
+            t_start: The starting time
+            t_end: The final time (best not set to zero for numerical stability)
+            steps: The number of steps for the solver
+            stochastic: Whether to use the SDE (True) or ODE (False)
+
+        Returns:
+            x_end: (Tensor of shape (batch_size, ...))
         """
+        device = x_start.device
+        x = x_start.detach().clone()
+
+        ts = batch_linspace(t_start, t_end, steps=steps).to(device)
+        delta_t = copy_tensor_or_create((t_end - t_start) / (steps - 1))  # Negative in reverse time
+
+        for t in ts:
+            score = self.score(x, t, **score_kwargs)
+            drift = self.drift(x, t)
+            diff = self.diff(t)
+
+            if t.ndim > 0:  # diff is batched, so add dimensions for broadcasting
+                new_dims = x.ndim - t.ndim
+                diff = diff.reshape(x.shape[:1] + (1,) * new_dims)
+                delta_t = delta_t.reshape(x.shape[:1] + (1,) * new_dims)
+
+            if stochastic:
+                # Perform an Euler-Maruyama step on the reverse SDE from equation (6)
+                delta_w = delta_t.abs().sqrt() * torch.randn(x.shape).to(device)
+                dx = (drift - diff**2 * score) * delta_t + diff * delta_w
+            else:
+                # Compute an Euler step on the reverse ODE from equation (13)
+                dx = (drift - diff**2 * score / 2) * delta_t
+
+            x += dx
+        return x
+
+class VpSDE(SDE):
+    """The variance-preserving SDE described by Song et al. (2020) in equation (11).
+
+    Here, the SDE is given by
+    dx = -(1/2) beta(t) x dt + sqrt(beta(t)) dw;
+    ie., f(x, t) = -(1/2)beta(t)x and g(t) = sqrt(beta(t)); where f(x, t) is the drift term and g(t) is the diffusion term.
+
+    For beta(t), we use a linear schedule: beta(t) = (beta_max - beta_min)*(t/T) + beta_min.
+
+    So, at t=0, beta(t) = beta_min and at t=T, beta(t) = beta_max. 
+
+    Beta controls how fast noise is added and signal decays. So in the initial stages of forward diffusion,
+    the noise is small and the signal is large. As time progresses, the noise increases and the signal decays.
+    """
+
+    def __init__(
+        self, 
+        score_net: nn.Module,
+        beta_min: float = 0.1,
+        beta_max: float = 20,
+        t_max: float = 1.0):
+
+        self.beta_min = torch.tensor(beta_min)
+        self.beta_max = torch.tensor(beta_max)
+        self.t_max = torch.tensor(t_max)
+
+        super().__init__(score_net)
+
+        
+    def drift(self, x, t):
+        """
+        The drift coefficient f(x, t) of the forward SDE.
+        f(x, t) = -(1/2) beta(t) x
+        """
+        beta_t = self.beta(t)
+        # it is negative because we are removing signal.
+        return -0.5 * beta_t * x
+    
+
+    def diff(self, t):
+        """
+        The diffusion coefficient g(t) of the forward SDE.
+        g(t) = sqrt(beta(t))
+        """
+        # it is positive because we are adding noise.
+        return torch.sqrt(self.beta(t))
+    
+
+    def beta(self, t):
+        """
+        The variance schedule beta(t) of the forward SDE, which is a linear one here.
+        It determines the amount of signal decay and noise addition at each time step.
+        beta(t) = (beta_max - beta_min) * (t / t_max) + beta_min
+        """
+        return (self.beta_max - self.beta_min) * t / self.t_max + self.beta_min
+    
+    
+    def beta_integral(self, t_start, t_end):
+        """
+        The integral of beta(t) from t_start to t_end.
+        This is used to compute the standard deviation (sigma).
+        """
+        return (self.beta(t_end) - self.beta(t_start)) * (t_end - t_start) / 2
+    
+
+    def sigma(self, t_end, t_start=0):
+        """The standard deviation of x(t_end) | x(t_start).
+        It is the scale (standard deviation) of the noise added during that interval of time (t_start, t_end).
+        This helps with computing the closed form solution of the forward SDE.
+        You use the sigma() function whenever you want to simulate the forward SDE in closed form,
+        i.e. not step by step in a numeric way.
+        """
+        return torch.sqrt(1.0 - torch.exp(-self.beta_integral(t_start, t_end)))
+    
+    def mu_scale(self, t_end, t_start=0.0):
+        """Scaling factor for the mean of x(t_end) | x(t_start).
+
+        The mean should equal mu_scale(t_end, t_start) * x(t_start)
+        """
+        return torch.exp(-self.beta_integral(t_start, t_end) / 2)
+    
+    def solve_forward_sde(self, x_start, t_end=1.0, t_start=0.0, return_eps=False):
+        """Solve the SDE forward from time t_start to t_end"""
+        t_start, t_end = self._match_timestep_shapes(t_start, t_end)
+        t_start, t_end = t_start.to(x_start.device), t_end.to(x_start.device)
+        assert torch.all(t_start <= t_end)
+
+        mu_scale = self.mu_scale(t_start=t_start, t_end=t_end)
+        sigma_end = self.sigma(t_start=t_start, t_end=t_end)
+        eps = torch.randn_like(x_start)
+
+        if mu_scale.ndim > 0:  # Add a broadcasting dimensions to the scalars
+            new_dims = x_start.ndim - mu_scale.ndim
+            mu_scale = mu_scale.reshape(x_start.shape[:1] + (1,) * new_dims)
+            sigma_end = sigma_end.reshape(x_start.shape[:1] + (1,) * new_dims)
+
+        x_end = mu_scale * x_start + sigma_end * eps
+
+        if return_eps:  # epsilon, the random noise value, may be needed for training
+            return x_end, eps
+        else:
+            return x_end
--- sde/utils.py
+++ sde/utils.py
@@ -13,6 +13,49 @@ def copy_tensor_or_create(t, **kwargs):
     if isinstance(t, numbers.Number):
         return torch.tensor(t, **kwargs)
     elif isinstance(t, torch.Tensor):
+        # This gives you a clean copy without messing up autograd or in-place issues.
         return t.clone().detach()
     else:
         raise ValueError(f"Cannot copy object of type {type(t)}")
+    
+
+
+def batch_linspace(start, end, steps):
+    """
+    Batched linspace function. If start and end are numbers, it will return
+    a linspace from start to end of size (steps, ). If start and end are
+    torch.Tensors that are of shape (batch_size, ) it will return a batch of
+    linspaces from start to end of size (steps, batch_size).
+
+    e.g. Example 1: Scalar input: batch_linspace(0.0, 1.0, 5), then returns tensor([0.0000, 0.2500, 0.5000, 0.7500, 1.0000])  # shape [5].
+    However, Example 2: Batched input
+
+    start = torch.tensor([0.0, 1.0])
+    end = torch.tensor([1.0, 3.0])
+    batch_linspace(start, end, 5)
+
+    tensor([
+    [0.0000, 1.0000],
+    [0.2500, 1.5000],
+    [0.5000, 2.0000],
+    [0.7500, 2.5000],
+    [1.0000, 3.0000],
+    ])  # shape [5, 2] → 5 time steps for 2 different linspaces
+
+
+    Args:
+        start: (torch.Tensor or float) The start of the linspace.
+        end: (torch.Tensor or float) The end of the linspace.
+        steps: (int) The number of steps in the linspace.
+    Returns:
+        (torch.Tensor) The linspace or batch of linspaces.
+    """
+    # Normal linspace behaviour
+    if isinstance(start, numbers.Number) or start.ndim == 0:
+        return torch.linspace(start, end, steps)
+
+    # Batched linspace behaviour
+    def linspace(start, end):
+        return start + torch.arange(0, steps).to(start.device) * (end - start) / (steps - 1)
+
+    return torch.vmap(linspace)(start, end).mT
