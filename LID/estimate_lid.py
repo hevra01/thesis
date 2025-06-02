3b4ea0648990ab2edf9682daec6d4f203ec92556
Hevra Petekkaya <hpetekka@slurm-submit-1.mpi-inf.mpg.de>
2025-06-02 12:57:08 +0200

    added the wandb code and class filter for mnist

--- LID/estimate_lid.py
+++ LID/estimate_lid.py
@@ -17,11 +17,13 @@ from fokker_planck_estimator import FlipdEstimator
 import argparse
 from models.mlp import MLPUnet
 from sde.sdes import VpSDE
+import wandb
 
 def estimate_LID_over_t_range(
     x,  # [data_dim] or [batch_size, data_dim]
     lid_estimator,
     t_values,
+    hutchinson_sample_count,
     ambient_dim,
     device,
     return_info=False,
@@ -30,27 +32,32 @@ def estimate_LID_over_t_range(
     """
     Estimates LID over a range of t values for a single instance or a batch.
     Returns the best LID using the knee algorithm.
+    This function is intended to be used alone, when the lid of a single instance 
+    or a batch is needed.
     """
     x = x.to(device)
     lid_curve = estimate_LID_over_t_range_batch(
             x=x,
             t_values=t_values,
+            hutchinson_sample_count=hutchinson_sample_count,
             lid_estimator=lid_estimator
         )
     lid_curve = np.array(lid_curve)
     t_values = np.array(t_values)
-    return compute_knee(
-        t_values, lid_curve, ambient_dim, return_info=return_info, **knee_kwargs
-    )
+    return lid_curve
 
 
-def estimate_LID_over_t_range_batch(x, t_values, lid_estimator):
+def estimate_LID_over_t_range_batch(x, t_values, hutchinson_sample_count, lid_estimator):
+    """
+    Performs LID estimation over a range of t values for a batch of data.
+    This function is used by the estimate_LID_over_t_range_dataloader function. 
+    """
     # this list will get filled with LID values for each t in t_values
     lid_curve = []
 
     # loop over each t value to estimate LID
     for t in t_values:
-        lid_vals = lid_estimator._estimate_lid(x, t=t)
+        lid_vals = lid_estimator._estimate_lid(x, t=t, hutchinson_sample_count=hutchinson_sample_count)
 
         # For a batch, we take the mean of LID values across the batch.
         # However, if the input is a single instance, we just take the value directly.
@@ -64,6 +71,7 @@ def estimate_LID_over_t_range_dataloader(
     dataloader,
     lid_estimator,
     t_values,
+    hutchinson_sample_count,
     ambient_dim,
     device,
     return_info=False,
@@ -90,6 +98,7 @@ def estimate_LID_over_t_range_dataloader(
         batch_lid_curve = estimate_LID_over_t_range_batch(
             x=images,
             t_values=t_values,
+            hutchinson_sample_count=hutchinson_sample_count,
             lid_estimator=lid_estimator
         )
         print(f"Batch LID Curve: {batch_lid_curve}")
@@ -105,10 +114,9 @@ def estimate_LID_over_t_range_dataloader(
     lid_curve = lid_curve_sum / lid_curve_count
     t_values = np.array(t_values)
 
-    # Use the knee algorithm to find the best LID estimate from the averaged curve
-    return compute_knee(
-        t_values, lid_curve, ambient_dim, return_info=return_info
-    )
+    return lid_curve
+
+    
 
 def main():
     parser = argparse.ArgumentParser(description="Estimate LID using Fokker-Planck Estimator")
@@ -119,20 +127,17 @@ def main():
     with open(args.config, "r") as f:
         cfg = json.load(f)
 
-    # # Initialize wandb if enabled
-    # if cfg["wand_enabled"]:
-    #     wandb.init(
-    #         project=cfg.wandb.project,
-    #         name=cfg.wandb.run_name,
-    #         mode=cfg.wandb.mode,
-    #         config=
-    #     )
+    # Initialize wandb if enabled
+    if cfg["wand_enabled"]:
+        os.environ["WANDB_API_KEY"] = "4fb9acb164293e106d3b4e0787abfb22bcfe9afa"
+        wandb.init(
+            project=cfg["wandb"]["project"],
+            name=cfg["wandb"]["run_name"],
+            mode=cfg["wandb"]["mode"],
+        )
 
     # Set device
     device = torch.device(cfg.get("device", "cuda" if torch.cuda.is_available() else "cpu"))
-
-    # Time at which to estimate LID. Theoretically, this should be a float value very close to zero.
-    t = cfg["t"] 
 
     # Prepare the model for score estimation, which will be used for the LID estimation.
     model_cfg = cfg["model"]
@@ -151,26 +156,28 @@ def main():
     # NOTE: this is a bit hard coded for MNIST, it needs to be more 
     # flexible to work with other datasets.
     # Initialize the dataset
-    dataloader = get_mnist_dataloader(flatten=True)
+    dataloader = get_mnist_dataloader(flatten=True, class_filter=cfg.get("class_filter"))
 
     # the range of t values over which to estimate LID
-    t_values = torch.linspace(0.15, 0.5, 50)
-    print(t_values)
+    t_values = torch.linspace(0.0, model.t_max, 100)
+    hutchinson_sample_count = cfg.get("hutchinson_sample_count")
 
     # Estimate LID over the range of t values for the entire dataset.
-    # Then use the knee algorithm to find the best LID estimate.
     
-    #knee_info = estimate_LID_over_t_range(next(iter(dataloader))["image"], lid_estimator,t_values, ambient_dim=model_cfg["data_dim"], device=device, return_info=True)
-    knee_info = estimate_LID_over_t_range_dataloader(dataloader, lid_estimator, 
-                                                   t_values, 
+    #lid_curve = estimate_LID_over_t_range(next(iter(dataloader))["image"], lid_estimator,t_values, ambient_dim=model_cfg["data_dim"], device=device, return_info=True)
+    lid_curve = estimate_LID_over_t_range_dataloader(dataloader, lid_estimator, 
+                                                   t_values, hutchinson_sample_count=hutchinson_sample_count,
                                                    ambient_dim=model_cfg["data_dim"],
                                                    device=device, return_info=True)
+
+     # Use the knee algorithm to find the best LID estimate from the averaged curve
+    knee_info = compute_knee(t_values, lid_curve, ambient_dim=model_cfg["data_dim"], return_info=True)
     
     print(knee_info["lid"], knee_info["knee_timestep"], knee_info["convex_hull"])
     # visualize the LID curve and knee point
     plot_lid_curve_with_knee(
-        knee_info["convex_hull"],
-        knee_info["timesteps"],
+        lid_curve,
+        t_values,
         knee_info["knee_timestep"],
         knee_info["lid"],
         save_path=cfg.get("save_path")
--- data/utils/dataloaders.py
+++ data/utils/dataloaders.py
@@ -3,7 +3,7 @@ from data.datasets.huggingface_dataset import HuggingFaceDataset
 from datasets import load_dataset
 from torch.utils.data import DataLoader
 
-def get_mnist_dataloader(batch_size=120, split="test", flatten=False):
+def get_mnist_dataloader(batch_size=120, split="test", flatten=False, class_filter=None):
     # Load raw MNIST from HuggingFace
     raw_dataset = load_dataset("mnist", split=split)
 
@@ -20,7 +20,7 @@ def get_mnist_dataloader(batch_size=120, split="test", flatten=False):
     transform = T.Compose(transform_list)
         
     # Wrap with your HuggingFaceDataset class
-    dataset = HuggingFaceDataset(dataset=raw_dataset, transform=transform)
+    dataset = HuggingFaceDataset(dataset=raw_dataset, class_filter=class_filter, transform=transform)
     # Create DataLoader
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
     return dataloader
