034588340857417acb886ec9358f85384909ec15
Hevra Petekkaya <hpetekka@slurm-submit-0.mpi-inf.mpg.de>
2025-07-07 18:11:37 +0200

    added the reconstructionDataset

--- data/utils/dataloaders.py
+++ data/utils/dataloaders.py
@@ -1,8 +1,13 @@
+import itertools
 import torchvision.transforms as T
 from data.datasets.huggingface_dataset import HuggingFaceDataset
 from datasets import load_dataset
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader, Dataset
+from dahuffman import HuffmanCodec
+
+
 
 def get_mnist_dataloader(batch_size=120, split="test", flatten=False, class_filter=None):
     # Load raw MNIST from HuggingFace
@@ -64,3 +69,93 @@ def get_imagenet_dataloader(
     # Wrap in DataLoader
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
     return dataloader
+
+class ReconstructionDataset(Dataset):
+    def __init__(self, reconstruction_data, all_registers, num_pixels, dataloader):
+        """
+        Args:
+            reconstruction_data (list): List of dicts containing reconstruction metrics.
+            all_registers (list): List of lists of register IDs (per image).
+            num_pixels (int): Number of pixels in each image (e.g., 256*256).
+            dataloader (DataLoader): Dataloader from which images can be fetched.
+        """
+        self.reconstruction_data = reconstruction_data
+        self.all_registers = all_registers
+        self.num_pixels = num_pixels
+        self.dataloader = dataloader    
+
+        # Create the Huffman codebook from the registers
+        # self.codebook will be a dictionary mapping register IDs to (bit_length, code_string)
+        self.flattened_all_registers = [r[0] if isinstance(r, list) and len(r) == 1 else r for r in all_registers]
+        flat_registers = list(itertools.chain.from_iterable(self.flattened_all_registers))
+
+        self.codebook = self.create_codebook(flat_registers)
+
+    def __len__(self):
+        # this will be the number of images * the number of k values for each image.
+        # e.g. for imagenet val, it is 50000 * len([1, 2, 4, 8, 16, 32, 64, 128, 180, 256]) = 500000
+        return len(self.reconstruction_data)
+
+    def __getitem__(self, idx):
+        """
+        Returns:
+            dict: Contains image ID, k value, MSE error, vgg error (or any other error) and compression rate.
+        """
+        data_point = self.reconstruction_data[idx]
+        image_id = data_point["image_id"]
+        k_value = data_point["k_value"]
+        mse_error = data_point["mse_error"]
+        vgg_error = data_point["vgg_error"]
+
+        # Compute compression rate based on Huffman codes
+        # Get the first k registers
+        registers = self.flattened_all_registers[image_id][:k_value] # this is bc we have nested list[[[registers]], [[registers]], etc]
+        
+        # get the lenths of the Huffman codes for these registers
+        # self.codebook is a dictionary mapping register IDs to (bit_length, code_string)
+        # so we can get the bit lengths for the registers used in this image
+        huffman_code_lengths = [self.codebook[register][0] for register in registers]
+
+        # Calculate the average bit length per pixel
+        bpp = sum(huffman_code_lengths) / self.num_pixels  
+
+        return {
+            "image": self.dataloader.dataset[image_id][0],  # Get the image from the dataloader
+            "k_value": k_value,
+            "mse_error": mse_error,
+            "vgg_error": vgg_error,
+            "bpp": bpp,  # bits per pixel
+        }
+    
+    def create_codebook(self, registers):
+        """
+        Create a Huffman codebook from the given registers.
+
+        Args:
+            registers (list): List of register IDs.
+
+        Returns:
+            dict: A dictionary mapping register IDs to (bit_length, code_string).
+        """
+        codec = HuffmanCodec.from_data(registers)
+
+        # Access the codebook directly
+        codebook = codec.get_code_table()  # Returns dict: symbol → (bit_length, code_string)
+
+        return codebook
+
+    def get_huffman_code(self, register):
+        """
+        Get the Huffman code for a given register ID.
+
+        Args:
+            register (int): The register ID.
+            codebook (dict): Huffman codebook mapping register IDs to (bit_length, code_string).
+
+        Returns:
+            str: Huffman code string for the register ID.
+        """
+        if register in self.codebook:
+            return self.codebook[register][1]  # Return the Huffman code string
+        else:
+            raise ValueError(f"Register ID {register} not found in the Huffman codebook.")
