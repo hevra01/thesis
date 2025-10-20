836fc9c19c4a9ff7cabc4c457d7c5ec2aaac61f3
Hevra Petekkaya <hpetekka@wks-21-0614>
2025-10-20 23:51:19 +0200

    code related to raven cluster

---
+++ cluster_related_raven/submit_reconstruct_with_tokens_array.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH -J reconstruct_tokens
+#SBATCH -o logs/out_%A_%a.log
+#SBATCH -e logs/err_%A_%a.log
+#SBATCH --time=02:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=16G
+#SBATCH --array=0-9
+
+# --- Environment setup ---
+module purge
+module load anaconda/3/2023.03
+source activate /u/hevrapetek/conda-envs/thesis
+
+# --- Configurable variables ---
+EXPERIMENT_NAME=${EXPERIMENT_NAME:-reconstruct_with_tokens}
+BASE_START_CLASS=${BASE_START_CLASS:-800}
+CLASSES_PER_JOB=${CLASSES_PER_JOB:-20}
+
+TASK_ID=${SLURM_ARRAY_TASK_ID:-0}
+START_CLASS=$(( BASE_START_CLASS + TASK_ID * CLASSES_PER_JOB ))
+END_CLASS=$(( START_CLASS + CLASSES_PER_JOB ))
+
+echo "[$(date)] Running task $TASK_ID: classes [$START_CLASS, $END_CLASS) for experiment=$EXPERIMENT_NAME"
+echo "SLURM_JOB_ID=$SLURM_JOB_ID on node $HOSTNAME"
+
+# --- Arguments for Python script ---
+ARGS=(
+  experiment=$EXPERIMENT_NAME
+  experiment.start_class_idx=$START_CLASS
+  experiment.end_class_idx=$END_CLASS
+  experiment.output_path=/ptmp/hevrapetek/reconstruction_imagenet/train
+  experiment.register_tokens_path=/ptmp/hevrapetek/thesis/data/datasets/imagnet_register_tokens/imagnet_train_register_tokens.npz
+  experiment.data_root=/ptmp/hevrapetek/train
+)
+
+# --- Run ---
+cd ~/thesis
+python -u reconstruct_with_tokens.py "${ARGS[@]}"
