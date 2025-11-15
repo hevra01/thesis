e2b2eb66351668ad7f0af382075daa07d6f01205
hevra01 <hevrapetekkaya01@gmail.com>
2025-11-15 17:38:47 +0100

    density using CFG

--- cluster_related_raven/submit_estimate_density_imgnet_2.sh
+++ cluster_related_raven/submit_estimate_density_imgnet_2.sh
@@ -21,9 +21,24 @@ EXPERIMENT_NAME=${EXPERIMENT_NAME:-estimate_density_RF_0_2}
 BASE_BATCH=${BASE_START_BATCH:-0}
 BATCHES_PER_JOB=${BATCHES_PER_JOB:-200} # check how many batches can fit into 1D per GPU
 
+# Allow sweeping over keep_k values via the same job array index.
+# Provide a space-separated list in KEEP_K_VALUES or use the default FlexTok sweep.
+KEEP_K_VALUES=${KEEP_K_VALUES:-"1 2 4 8 16 32 64 128 256"}
+read -r -a KEEP_K_ARRAY <<< "$KEEP_K_VALUES"
+NUM_K=${#KEEP_K_ARRAY[@]}
+
 TASK_ID=${SLURM_ARRAY_TASK_ID:-0}
-START_BATCH=$(( BASE_BATCH + TASK_ID * BATCHES_PER_JOB ))
+
+# Map the 1D array index into (k_index, batch_block_index):
+#   k_index varies fastest, batch block varies slowest.
+k_index=$(( TASK_ID % NUM_K ))
+block_index=$(( TASK_ID / NUM_K ))
+
+START_BATCH=$(( BASE_BATCH + block_index * BATCHES_PER_JOB ))
 END_BATCH=$(( START_BATCH + BATCHES_PER_JOB ))
+KEEP_K=${KEEP_K_ARRAY[$k_index]}
+
+echo "[submit] TASK_ID=$TASK_ID -> k_index=$k_index keep_k=$KEEP_K, block_index=$block_index, start=$START_BATCH end=$END_BATCH"
 
 
 # ---------------- Run your code ---------------- #
@@ -34,8 +49,9 @@ ARGS=(
   experiment=$EXPERIMENT_NAME
   experiment.start_batch_idx=$START_BATCH
   experiment.end_batch_idx=$END_BATCH
-  experiment.output_path=/ptmp/hevrapetek/thesis/data/datasets/density_imagenet_val/corrected_hutchinson_4/token_count_1/
-  experiment.keep_k=1
+  experiment.output_path=/ptmp/hevrapetek/thesis/data/datasets/density_imagenet_val/analysis_cfg/token_count_${KEEP_K}/
+  experiment.keep_k=${KEEP_K}
+  experiment.timesteps=50
   experiment.register_path=/ptmp/hevrapetek/thesis/data/datasets/imagnet_register_tokens/imagnet_val_register_tokens.npz
   experiment.dataset.root=/ptmp/hevrapetek/ILSVR2012/
   experiment.dataset.split="val_categorized"
