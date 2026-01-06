4b86a2412cfff604984b4167376cd4bb88e096bd
hevra01 <hevrapetekkaya01@gmail.com>
2026-01-06 16:10:38 +0100

    submit file for reconstruction loss estimation

--- cluster_related_raven/submit_estimate_reconstruction_loss.sh
+++ cluster_related_raven/submit_estimate_reconstruction_loss.sh
@@ -2,7 +2,7 @@
 #SBATCH -J reconstruct_tokens
 #SBATCH -o /ptmp/hevrapetek/thesis/logs/current.out
 #SBATCH -e /ptmp/hevrapetek/thesis/logs/current.err
-#SBATCH --time=0-10:00:00
+#SBATCH --time=0-8:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
@@ -19,15 +19,24 @@ source /ptmp/hevrapetek/thesis/.wandb_secrets.sh
 
 cd /ptmp/hevrapetek/thesis
 
+# --- Pick k based on Slurm job-array index ---
+# Submit with: sbatch --array=0-8 this_script.sh
+k_keep_list=(1 2 4 8 16 32 64 128 256)
+
+JOB_INDEX=${SLURM_ARRAY_TASK_ID:-0}
+K=${k_keep_list[$JOB_INDEX]}
+
+echo "SLURM_ARRAY_TASK_ID=${JOB_INDEX} -> K=${K}"
+
 # --- Arguments for Python script ---
 ARGS=(
   experiment=${EXPERIMENT_NAME:-estimate_reconstruction_loss}
-  experiment.reconstructed_data_path=/ptmp/hevrapetek/reconstruction_imagenet/val
-  experiment.reconstruction_loss_output_path=/ptmp/hevrapetek/thesis/data/datasets/imagenet_reconstruction_losses/val/all.json
-  experiment.register_token_path="/ptmp/hevrapetek/thesis/data/datasets/imagnet_register_tokens/imagnet_val_register_tokens.npz"
+  experiment.reconstructed_data_path=/ptmp/hevrapetek/reconstruction_imagenet_APC_true/train
+  experiment.reconstruction_loss_output_path=/ptmp/hevrapetek/thesis/data/datasets/imagenet_reconstruction_losses/train/${K}.json
+  experiment.register_token_path="/ptmp/hevrapetek/thesis/data/datasets/imagnet_register_tokens/imagnet_train_register_tokens.npz"
   experiment.dataset.root="/ptmp/hevrapetek/ILSVR2012"
-  experiment.dataset.split="val_categorized"
-  experiment.k_keep_list=[1, 2, 3]
+  experiment.dataset.split="train"
+  experiment.k_keep_list=[${K}]
 )
 
 python find_reconstruction_loss.py "${ARGS[@]}"
